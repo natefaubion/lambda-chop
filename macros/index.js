a16943f59bd7e82d8cbe8c124dea60c24d052e4b
Nathan Faubion <nathan@n-son.com>
2014-01-03 17:10:22 -0600

    Make sure everything gets a context

--- macros/index.js
+++ macros/index.js
@@ -12,19 +12,19 @@ macro $lc__curry {
     var body = #{ $body ... };
 
     if (!args.length) {
-      args.push(makeDelim('()', []));
+      args.push(makeDelim('()', [], here));
     }
 
     return args.reduceRight(function(bod, alist, i) {
-      var func = makeKeyword('function');
-      var ret  = i > 0 ? [makeKeyword('return', func)] : [];
-      var code = ret.concat(func, alist, makeDelim('{}', bod));
+      var func = makeKeyword('function', here);
+      var ret  = i > 0 ? [makeKeyword('return', here)] : [];
+      var code = ret.concat(func, alist, makeDelim('{}', bod, here));
 
       return symbol[0].token.value === '=>'
         ? code.concat(
-            makePunc('.'), 
-            makeIdent('bind'), 
-            makeDelim('()', [makeKeyword('this')]))
+            makePunc('.', here), 
+            makeIdent('bind', here), 
+            makeDelim('()', [makeKeyword('this', here)], here))
         : code;
     }, body);
   }
@@ -35,9 +35,9 @@ macro $lc__placeholders {
     var here = #{ here };
     var res = makePlaceholders(#{ $body ... });
     return [
-      makeKeyword('function'),
+      makeKeyword('function', here),
       makeDelim('()', res[1], here),
-      makeDelim('{}', [makeKeyword('return', res[0][0])].concat(res[0]))
+      makeDelim('{}', [makeKeyword('return', res[0][0])].concat(res[0]), here)
     ];
 
     function makePlaceholders(stx) {
@@ -50,7 +50,7 @@ macro $lc__placeholders {
           if (s.token.type === parser.Token.Punctuator &&
               s.token.value === '#') {
             var ident = makeIdent(String.fromCharCode(code++), here);
-            if (args.length) args.push(makePunc(','));
+            if (args.length) args.push(makePunc(',', here));
             args.push(ident);
             return ident;
           }
